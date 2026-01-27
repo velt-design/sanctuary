e00a3c5b6eea3138bba6f79d538a1f35233ec604
Jordan Breetvelt <velt_mac@Jordans-MacBook-Pro.local>
2026-01-27 17:12:14 +1100

    lalala

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
         "style-src 'self' 'unsafe-inline'",
         "img-src 'self' data: blob: https://www.google-analytics.com https://www.sanctuarypergolas.co.nz https://www.facebook.com https://stats.g.doubleclick.net https://www.googleadservices.com",
         "font-src 'self' data:",
-        "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://www.googleadservices.com https://www.facebook.com https://graph.facebook.com https://pixel.archipro.co.nz",
+        "connect-src 'self' https://www.google-analytics.com https://region1.google-analytics.com https://analytics.google.com https://stats.g.doubleclick.net https://www.googleadservices.com https://www.facebook.com https://graph.facebook.com https://pixel.archipro.co.nz https://*.supabase.co wss://*.supabase.co",
         "frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com",
         "object-src 'none'",
         "base-uri 'self'",
